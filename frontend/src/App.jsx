f9f370b9236fe1639740c1aa76680183400122d9
philips-of-ng@philips <philipsedun@gmail.com>
2024-10-24 15:06:28 +0100

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import About from './pages/about'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<Signup />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
