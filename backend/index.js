57a7b382d5e7c61f4c71326ebda3b0c34c4555cc
philips-of-ng@philips <philipsedun@gmail.com>
2024-10-27 01:08:53 +0100

    okayyy

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,16 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config()
+
+const db_password = process.env.DB_PASSWORD
+
+mongoose.connect(`mongodb+srv://philipsedun:${db_password}@mern-blog.ej4hh.mongodb.net/?retryWrites=true&w=majority&appName=mern-blog`)
+  .then(() => console.log('MongoDB Connected Successfully'))
+  .catch((error => console.log(error)))
+
+  
 const app = express()
 
 const PORT = 3000
